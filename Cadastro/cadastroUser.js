33d2f19fdc45b5ee607fdd30e88515d1e2d36140
Jabutiaquaticokk <joseesdrasandradearaujo@gmail.com>
2026-07-26 10:31:02 -0300

    sistema de cadastro e login 72%

--- Cadastro/cadastroUser.js
+++ Cadastro/cadastroUser.js
@@ -1,7 +1,15 @@
-const usuarios=[]
+const usuarios=[{
+    id: 1785072358251,
+    user: 'José',
+    email: 'jose_reidelas67@gmail.com',
+    telefone: '57 4788-8686',
+    senha: 'cOxinha123',
+},
+]
 
 function cadastrar(){
     let usuario={
+        id: Date.now(),
         user: document.getElementById('input-novoUsuario').value,
         email: document.getElementById('input-email').value,
         telefone: document.getElementById('input-telefone').value,
